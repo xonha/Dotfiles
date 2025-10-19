e2d8a6689273e1cf4dc98190bbc2059e81d750bd
Henrique Oliveira <henrique.oliveira@maistodos.com.br>
2025-10-19 16:21:25 -0300

    neovim

--- .config/nvim/lua/plugins/lua_snip.lua
+++
@@ -1,6 +0,0 @@
-return {
-  "L3MON4D3/LuaSnip",
-  config = function()
-    require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/lua/snippets/" })
-  end,
-}
--- .config/nvim/lua/plugins/neo_tree.lua
+++ .config/nvim/lua/plugins/neo_tree.lua
@@ -2,7 +2,7 @@ return {
   "nvim-neo-tree/neo-tree.nvim",
   opts = {
     window = {
-      position = "left",
+      position = "right",
     },
     event_handlers = {
       {
