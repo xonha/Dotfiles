9c40920335e4721bbbce267804442eec158c21a7
Henrique Oliveira <ho.slouch@gmail.com>
2026-03-02 13:59:30 -0300

    style: add Catppuccin theme configuration with mocha flavor and color overrides

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,5 +1,25 @@
 return {
   {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    opts = {
+      flavour = "mocha",
+      color_overrides = {
+        mocha = {
+          base = "#151515",
+          crust = "#0f0f0f",
+          mantle = "#121212",
+          overlay0 = "#3f3f3f",
+          overlay1 = "#4f4f4f",
+          overlay2 = "#5f5f5f",
+          surface0 = "#1c1c1c",
+          surface1 = "#232323",
+          surface2 = "#2a2a2a",
+        },
+      },
+    },
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "catppuccin-mocha",
